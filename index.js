73c03b770dd01bcb65129b2255029d3f389762b9
bhageera87 <patilabhishek6705@gmail.com>
2025-02-10 12:28:22 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { SidebarProvider } from './context/sidebarContext';
-import { MealProvider } from './context/mealContext';
+import Home from "../pages/HomePage/HomePage";
+import Error from "../pages/ErrorPage/ErrorPage";
+import MealDetails from "../pages/MealDetailsPage/MealDetailsPage";
+import Category from "../pages/CategoryPage/CategoryPage";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <SidebarProvider>
-    <MealProvider>
-      <App />
-    </MealProvider>
-  </SidebarProvider>
-);
-
+export { Home, Error, MealDetails, Category };
